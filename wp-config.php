3ed62a3dbe81241469b5036b45d59f098965c251
AKSHAY N <princeakshay150@gmail.com>
2019-07-13 15:17:35 +0530

    posts-update

--- wp-config.php
+++ wp-config.php
@@ -88,3 +88,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+/**Execution Time Extended**/
+set_time_limit(300);
